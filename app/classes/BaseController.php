c9d1dadc2f41e89d505f6257296a764b87820e64
Cloudermaan <stefan.pasat@gmail.com>
2013-05-30 19:36:53 +0300

    Update BaseController.php
    fixed to be string

--- app/classes/BaseController.php
+++ app/classes/BaseController.php
@@ -285,7 +285,7 @@ class BaseController extends RExtController {
 	
 	protected function _logFile($db, $collection) {
 		$logDir = dirname(__ROOT__) . DS . "logs";
-		return $logDir . DS . urlencode(MServer::serverWithIndex($this->_admin->hostIndex())) . "-query-" . urlencode($db) . "-" . urlencode($collection) . ".php";
+		        return $logDir . DS . urlencode(MServer::serverWithIndex($this->_admin->hostIndex())->mongoName()) . "-query-" . urlencode($db) . "-" . urlencode($collection) . ".php";
 	}
 	
 	/**
